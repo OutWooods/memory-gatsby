3ce36a4170e333098036e20d979e706fcf7bdd0c
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2020-06-21 19:47:34 +0100

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { PageProps } from "gatsby"
+import React from "react"
+
+const Error404Page: React.FC<PageProps> = () => (
+    <div>
+    <h1>You are here!</h1>
+    <h2>But nothing found for you #404</h2>
+    </div>
+)
+
+export default Error404Page
