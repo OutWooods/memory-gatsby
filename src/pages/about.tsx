b00a89d4376d9bde80dc851fdf529b054be5174b
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2020-06-21 19:48:37 +0100

    change base pages to tsx

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { PageProps } from "gatsby"
+
+export default function IndexRoute(props: PageProps) {
+  return (
+    <div style={{ color: `teal` }}>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
