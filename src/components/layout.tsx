ddfd2523edc8fd0b6ce14d08d6dff244dc75c3ac
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2020-06-27 10:06:31 +0100

    change layout components to typescript

---
+++ src/components/layout.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import { Link } from 'gatsby';
+import ListLink from './listLink';
+
+export interface LayoutProps {
+    children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps): JSX.Element {
+    return (
+        <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+            <header style={{ marginBottom: `1.5rem` }}>
+                <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+                    <h3 style={{ display: `inline` }}>MySweetSite</h3>
+                </Link>
+                <ul style={{ listStyle: `none`, float: `right` }}>
+                    <ListLink to="/">Home</ListLink>
+                    <ListLink to="/about/">About</ListLink>
+                    <ListLink to="/contact/">Contact</ListLink>
+                </ul>
+            </header>
+            {children}
+        </div>
+    );
+}
