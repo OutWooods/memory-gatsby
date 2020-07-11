4f3bd09d35f44c9ade262c113b626dd96a5bafed
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2020-07-11 15:51:20 +0100

    WIP add example context

---
+++ src/provider.ts
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+export interface ExampleProps {
+    examples: number[],
+}
+
+export const WithExampleContext = React.createContext<ExampleProps>({
+    examples: [],
+});
+
+const ExampleContext = ({ children }: { children: JSX.Element }) => {
+    const [examples] = useState([]);
+
+    const value = { examples };
+
+    return <WithExampleContext.Provider value={value}>{children}</WithExampleContext.Provider>;
+};
+
+// eslint-disable-next-line react/display-name
+export default ({ element }: { element: JSX.Element }): JSX.Element => <ExampleContext>{element}</ExampleContext>;
